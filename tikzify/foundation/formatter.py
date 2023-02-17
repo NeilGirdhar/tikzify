ea8340070fc387be247ca3f1cdf8c080f93aa7f4
Neil Girdhar <mistersheik@gmail.com>
2023-02-17 18:31:05 -0500

    Support line continuation

--- tikzify/foundation/formatter.py
+++ tikzify/foundation/formatter.py
@@ -7,9 +7,10 @@ __all__ = ['formatter']
 
 
 def formatter(string_to_be_printed: str, **kwargs: Any) -> str:
-    """
-    Perform recursive string formatting on a string.  Any keywords enclosed in curly quotation marks
-    are expanded using the keyword arguments passed into the function with a few details:
+    """Perform recursive string formatting on a string.
+
+    Any keywords enclosed in curly quotation marks are expanded using the keyword arguments passed
+    into the function with a few details:
     * The formatter is applied recursively to the expanded string.  E.g.,
       formatter("“a”", a="expanded “b”", b="expanded c")
       returns
@@ -26,7 +27,8 @@ def formatter(string_to_be_printed: str, **kwargs: Any) -> str:
       formatter("“a:.3f, b:3d”", a=1.23456, b=7)
       returns
       '1.235, 007'
-    Finally, the returned string is unindented and stripped of whitespace at either end.
+    Finally, the returned string is unindented, stripped of whitespace at either end, and line
+    continuations (using backslash) are applied.
     """
     def repl(m: Match[str]) -> str:
         keyword = m.group(1)
@@ -45,7 +47,10 @@ def formatter(string_to_be_printed: str, **kwargs: Any) -> str:
         return ", ".join(retval)
 
     try:
-        retval = re.sub(r"“(.+?)”", repl, dedent(string_to_be_printed)).strip('\n')
+        expanded_macros = re.sub(r"“(.+?)”", repl, string_to_be_printed)
+        dedented = dedent(expanded_macros)
+        stripped = dedented.strip('\n')
+        retval = re.sub("\\\\\n\s*", "", stripped)
     except KeyError as e:
         raise Exception(f"No key \"{e.args[0]}\" found in {kwargs} for formatted string "
                         f"{string_to_be_printed}.") from None
