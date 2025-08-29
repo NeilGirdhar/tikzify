ef6028403e1dbeca4cec91f843828ef177e10857
Neil Girdhar <mistersheik@gmail.com>
2025-08-29 02:53:31 -0400

    Delete dead code

--- tikzify/__init__.py
+++ tikzify/__init__.py
@@ -1,22 +1,13 @@
 from ._src.foundation.context import Context
 from ._src.foundation.elements import Element, Import, Math, TypstObject
 from ._src.foundation.function import CalledFunction, Function, function
-from ._src.function_graph.annotation import (Annotation, BraceAnnotation, CircleAnnotation,
-                                             EdgeAnnotation, RectAnnotation)
 from ._src.function_graph.curve_generator import generate_curve
 from ._src.function_graph.curve_source.curve_source import CurveSource
 from ._src.function_graph.curve_source.function import FunctionCurveSource, FunctionSection
 from ._src.function_graph.curve_source.trajectory import TrajectoryCurveSource
-from ._src.function_graph.draw import draw_curve, function_graph_line, function_graph_marks
 from ._src.function_graph.graph import FunctionGraph
-from ._src.function_graph.legend import (LegendArrow, LegendItem, LegendNode, LegendRect,
-                                         generate_legend)
-from ._src.function_graph.multi_graph import FunctionMultiGraph
 from ._src.function_graph.trait import GraphedTrait
 
-__all__ = ['Annotation', 'BraceAnnotation', 'CalledFunction', 'CircleAnnotation', 'Context',
-           'CurveSource', 'EdgeAnnotation', 'Element', 'Function', 'FunctionCurveSource',
-           'FunctionGraph', 'FunctionMultiGraph', 'FunctionSection', 'GraphedTrait', 'Import',
-           'LegendArrow', 'LegendItem', 'LegendNode', 'LegendRect', 'Math', 'RectAnnotation',
-           'TrajectoryCurveSource', 'TypstObject', 'draw_curve', 'function', 'function_graph_line',
-           'function_graph_marks', 'generate_curve', 'generate_legend']
+__all__ = ['CalledFunction', 'Context', 'CurveSource', 'Element', 'Function', 'FunctionCurveSource',
+           'FunctionGraph', 'FunctionSection', 'GraphedTrait', 'Import', 'Math',
+           'TrajectoryCurveSource', 'TypstObject', 'function', 'generate_curve']
